97d12dd5dcf02413717c4156862125a28967ffd3
Ankit Raj <ankittrajj1996@gmail.com>
2022-02-19 00:05:23 -0600

    plz work on option no 3 & 4

--- Bank_project.py
+++ Bank_project.py
@@ -21,7 +21,7 @@ cursor = con.cursor()
 
 while True:
     print("Bank Management System\n")
-    choice = input("1. Open Account\n2.Cash Deposit\n3.Cash Withdrawl\n4.Update Acc\n5.Account Details\n6.Exit")
+    choice = input("1. Open Account\n2.Cash Deposit\n3.Cash Withdrawl\n4.Account Details\n5.Exit")
     # open account
     if choice == '1':
         name = input("Enter the customer name")
@@ -34,15 +34,43 @@ while True:
         con.commit()
         print('Account Opened!!')
 
+    # cash deposit
     elif choice == '2':
         name = input('Enter the name of acc:-')
-        amt = int(input("Enter the amt to deposit:-"))
-        query = "update cust set amount={} where name='{}'".format(amt,name)
+        deposit_amt = int(input("Enter the amt to deposit:-"))
+        query = "update cust set amount={} where name='{}'".format(deposit_amt,name)
         cursor.execute(query)
         con.commit()
         print("Amount Deposit")
 
+    # 3.cash withdrawl------> if amount>curramt---->print('insufficient balance')
+    # 4. update ----> cash withdrwal + cash deposit------> remaining amt.
 
-    #   exit
-    elif choice =='6':
+    # cash withdrawl
+    # elif choice == '3':
+    #     name = input('Enter the name of acc:-')
+    #     with_amt = int(input("Enter the amt to withdraw:-"))
+    #     # deposit_amt =amount
+    #     if with_amt>deposit_amt:
+    #         print("Insufficient Balance")
+    #     else:
+    #         # print("Amount Withdrwal")
+    #         query = "update cust set amount={} where name='{}'".format(with_amt,name)
+    #         cursor.execute(query)
+    #         con.commit()
+
+    # 5 acc details
+    elif choice=='4':
+        name = input("Enter the acc name:-")
+        query = "select * from cust where name='{}'".format(name)
+        cursor.execute(query)
+        data = cursor.fetchall()
+        print(data)
+        con.commit()
+
+    # exit
+    elif choice =='5':
         break;
+
+
+    # 1 bank sytem,hotel sytem,school management,hospital system.
